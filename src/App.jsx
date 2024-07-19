a75f99ed6c29d00d3daf4127622ec81d01f22d7b
Gilbert Haro <gilbertharocode@gmail.com>
2024-07-19 01:24:45 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Extracting State Logic into a Reducer</h1>
+      <h3>reduce this complexity </h3>
+    </>
+  );
+}
+
+export default App;
